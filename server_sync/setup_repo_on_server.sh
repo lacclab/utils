e049413985d2c1a61dfccb13753fdad8deef4cd1
Shubi (srv2) <omer.shubi@gmail.com>
2024-04-16 13:43:26 +0300

    Refactor setup_repo_on_server.sh script to improve command-line argument handling and error checking

--- server_sync/setup_repo_on_server.sh
+++ server_sync/setup_repo_on_server.sh
@@ -1,34 +1,45 @@
 #!/bin/bash
-# This script sets up a new server by installing conda, cloning a git repository, and setting up a conda environment.
-# It takes optional command-line arguments:
-# 1. The URL of the repository to clone (default is "https://github.com/user/repo.git").
-# 2. Flag to skip downloading and installing conda (default is false).
-# 3. Flag to skip cloning the repository (default is false).
-# 4. The name of the environment file (default is "environment.yml").
-# Creates a conda environment if the specified environment file exists in the repository.
-#
-# Usage:
-# ./setup_new_server.sh [repo_url] [--skip-conda] [--skip-clone] [--environment-file env_file]
-#
+# Script Name: setup_repo_on_server.sh
+# Description: This script sets up a new server by cloning a git repository, and setting up a conda environment.
+#              It takes optional command-line arguments:
+#              1. The URL of the repository to clone (default is "https://github.com/user/repo.git").
+#              2. The name of the environment file (default is "environment.yml").
+#              Creates a conda environment if the specified environment file exists in the repository.
+# Usage: ./setup_new_server.sh [repo_url] [--env-file env_file]
 # Examples:
 # ./setup_new_server.sh
 # ./setup_new_server.sh https://github.com/lacclab/Cognitive-State-Decoding.git
-# ./setup_new_server.sh https://github.com/lacclab/Cognitive-State-Decoding.git --skip-conda
-# ./setup_new_server.sh https://github.com/lacclab/Cognitive-State-Decoding.git --skip-clone
-# ./setup_new_server.sh https://github.com/lacclab/Cognitive-State-Decoding.git --environment-file my_env.yml
-#
-# To run the script directly from GitHub:
-# git clone https://gist.github.com/814eb6a9a8b59febbcfcc79288630b60.git code_setup && bash code_setup/setup_new_server.sh https://github.com/lacclab/Cognitive-State-Decoding.git
+# ./setup_new_server.sh https://github.com/lacclab/Cognitive-State-Decoding.git --env-file my_env.yml
 
 # Exit if any command fails
 set -e
 
 # Default values
-default_repo="https://github.com/user/repo.git"
+repo_path="https://github.com/user/repo.git"
 environment_file="environment.yml"
+
+# Parse command-line arguments
+while [[ $# -gt 0 ]]; do
+  case $1 in
+  --env-file)
+    environment_file="$2"
+    shift 2
+    ;;
+  *)
+    repo_path="$1"
+    shift
+    ;;
+  esac
+done
 
 # Hardcode conda_path to miniforge3 directory in home directory
 conda_path="${HOME}/miniforge3"
+
+# Check if conda and mamba are installed
+if [ ! -d "$conda_path" ]; then
+  echo "Conda and mamba are not installed in the expected location: $conda_path"
+  exit 1
+fi
 
 # Activate conda and mamba without opening a new terminal
 source "${conda_path}/etc/profile.d/conda.sh"
@@ -42,10 +53,13 @@ repo_name=$(basename $repo_path | sed -e 's/.git$//')
 echo "Repo path: $repo_path"
 echo "Repo name: $repo_name"
 
-git clone $repo_path
-
-# Change directory to the cloned repository
-cd $repo_name
+# Clone the repository
+if git clone $repo_path; then
+  cd $repo_name
+else
+  echo "Failed to clone repository: $repo_path"
+  exit 1
+fi
 
 # Create a conda environment if the specified environment file exists
 if [ -f "$environment_file" ]; then
@@ -54,5 +68,3 @@ else
   # Print a message if the environment file is not found
   echo "$environment_file not found. Environment not set up."
 fi
-
-python -m spacy download en_core_web_sm
