92fd8757b09528bd2bf83b4de25f0596e0e56485
Shubi (srv2) <omer.shubi@gmail.com>
2024-04-16 13:36:58 +0300

    Refactor server_sync/sync_data_all_servers.sh script to use server_list.txt for server synchronization

--- server_sync/sync_data_all_servers.sh
+++ server_sync/sync_data_all_servers.sh
@@ -4,12 +4,9 @@
 data_path="/data/home/shared/onestop/processed"
 
 user=$(whoami)
-for i in {08..20}; do
+while IFS= read -r server; do
+    echo "Synchronizing data with $user@$server.iem.technion.ac.il:$data_path"
     ssh $user@nlp$i.iem.technion.ac.il "mkdir -p $data_path"
-    rsync -avzP $data_path/ $user@nlp$i.iem.technion.ac.il:$data_path # --include='*.tsv' --exclude='*'
-done
+    rsync -avzP $data_path/ $user@$server.iem.technion.ac.il:$data_path # --include='*.tsv' --exclude='*'
+done < server_sync/server_list.txt
 
-for i in {1..2}; do
-    ssh $user@nlp-srv$i.iem.technion.ac.il "mkdir -p $data_path"
-    rsync -avzP $data_path/ $user@nlp-srv$i.iem.technion.ac.il:$data_path # --include='*.tsv' --exclude='*'
-done
