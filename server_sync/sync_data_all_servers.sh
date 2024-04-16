3d1ff947d6e5876b66c236f886857112319d6cd9
kfir-hadar (nlp16) <cfir.hadar@gmail.com>
2024-04-16 21:11:31 +0300

    working

--- server_sync/sync_data_all_servers.sh
+++ server_sync/sync_data_all_servers.sh
@@ -6,7 +6,6 @@ data_path="/data/home/shared/onestop/processed"
 user=$(whoami)
 while IFS= read -r server; do
     echo "Synchronizing data with $user@$server.iem.technion.ac.il:$data_path"
-    ssh $user@$server.iem.technion.ac.il "mkdir -p $data_path"
-    rsync -avzP $data_path/ $user@$server.iem.technion.ac.il:$data_path # --include='*.tsv' --exclude='*'
+    rsync --mkpath -avzP $data_path/ $user@$server.iem.technion.ac.il:$data_path --exclude='*.pkl'  # --include='*.tsv' --exclude='*'
 done < server_sync/server_list.txt
 
